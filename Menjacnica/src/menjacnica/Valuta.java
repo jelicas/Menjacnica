8c50acdd181f83c77ac62a104c6337665c7d5502
jelicastanojevic <jelicastanojevicc@gmail.com>
2017-03-29 19:44:40 +0200

    Dodate equals, hashCode, toString metode i konstruktor u klasi Valuta

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -10,6 +10,17 @@ public class Valuta {
 	private double srednjiKurs;
 	private GregorianCalendar datum;
 	
+	public Valuta(String naziv, String skraceniNaziv, double prodajniKurs, double kupovniKurs, double srednjiKurs,
+			GregorianCalendar datum) {
+		super();
+		this.naziv = naziv;
+		this.skraceniNaziv = skraceniNaziv;
+		this.prodajniKurs = prodajniKurs;
+		this.kupovniKurs = kupovniKurs;
+		this.srednjiKurs = srednjiKurs;
+		this.datum = datum;
+	}
+	
 	public String getNaziv() {
 		return naziv;
 	}
@@ -45,6 +56,59 @@ public class Valuta {
 	}
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Valuta))
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", prodajniKurs=" + prodajniKurs
+				+ ", kupovniKurs=" + kupovniKurs + ", srednjiKurs=" + srednjiKurs + ", datum=" + datum + "]";
 	}
 	
 	
