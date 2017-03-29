df4477e01f4250234111d4ee88f7d789279a3b95
jelicastanojevic <jelicastanojevicc@gmail.com>
2017-03-29 20:11:51 +0200

    Uvedena klasa Menjacnica u paketu menjacnica koja implementira MenjacnicaInterfejs sa praznim telima metoda

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import valuta.Valuta;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValuteZaOdredjeniDan(String naziv, String skraceniNaziv, double prodajniKurs,
+			double kupovniKurs, double srednjiKurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteZaOdredjeniDan(String naziv, String skraceniNaziv, double prodajniKurs,
+			double kupovniKurs, double srednjiKurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta pronadjiIVratiKursValuteZaOdredjeniDan(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
